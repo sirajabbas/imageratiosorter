fd6701e2da0d7c12a971fd3d3fcbf63d4471fbe7
siraj <siraj.ea@padath.com>
2021-01-16 12:47:09 +0530

    feature added: sorted image copied to respective ratio directory

--- file/file.go
+++ file/file.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	_ "image/png"
+	"io"
 	"log"
 	"math/big"
 	"os"
@@ -13,12 +15,10 @@ import (
 	"github.com/sirajabbas/utilsgo"
 )
 
-var AllowedRatios = []*big.Rat{
-	new(big.Rat).SetFrac64(1, 1),  //1:1
-	new(big.Rat).SetFrac64(4, 3),  //4:3
-	new(big.Rat).SetFrac64(3, 4),  //3:4
-	new(big.Rat).SetFrac64(16, 9), //16:9
-}
+//variable to hold the filtering rations
+var AllowedRatios = []*big.Rat{}
+
+//variable to hold the allowed file extensions
 var AllowedFileTypes = []string{"jpg", "jpeg", "png"}
 
 type File struct {
@@ -28,7 +28,7 @@ type File struct {
 
 var files []File
 
-func SortFiles(path string) {
+func SortFiles(path, destination string) {
 	counter := 0
 	err := filepath.Walk(path,
 		func(fpath string, info os.FileInfo, err error) error {
@@ -50,13 +50,12 @@ func SortFiles(path string) {
 			return nil
 		})
 	if err != nil {
-		utilsgo.SugarLogger.Error(err)
-		log.Fatal("Error reading files from directory.")
+		log.Fatal("Error reading files from directory.", err)
 	}
 	for _, f := range files {
-		w, h, err := GetImageDimension(f.Path)
+		w, h, err := getImageDimension(f.Path)
 		if err != nil {
-			utilsgo.SugarLogger.Error("Error reading image dimention: ", f.Path, err)
+			log.Println("Error reading image dimention: ", f.Path, err)
 			continue
 		}
 		ratio := big.NewRat(int64(w), int64(h))
@@ -64,38 +63,65 @@ func SortFiles(path string) {
 		for _, r := range AllowedRatios {
 			if r.String() == ratio.String() {
 				isAllowed = true
+				//move the file to respective folder
+				err = f.copyFile(fmt.Sprintf("%s/%s", destination, strings.Replace(r.String(), "/", ":", 1)))
+				if err != nil {
+					fmt.Println(fmt.Sprintf("Error copying file %s", f.Path))
+				}
 				break
 			}
 		}
 		if !isAllowed {
 			//move file to unsorted directory
-			err := os.Rename(f.Path, fmt.Sprintf("%s/unsorted/%s", path, f.Name))
+			err := f.copyFile(fmt.Sprintf("%s/%s", destination, "unsorted"))
 			if err != nil {
-				utilsgo.SugarLogger.Error("error moving file to unsorted directory")
+				log.Println("error moving file to unsorted directory", err)
 				continue
 			}
 			counter++
 		}
 	}
-	utilsgo.PrintSuccess(fmt.Sprintf("%d files are identified and moved to unsorted directory", counter))
+	utilsgo.PrintSuccess(fmt.Sprintf("%d files are unidentified and moved to unsorted directory", counter))
+	utilsgo.PrintSuccess("done")
 
 }
 
-func GetImageDimension(filePath string) (w, h int, error error) {
+func getImageDimension(filePath string) (w, h int, error error) {
 	if reader, err := os.Open(filePath); err == nil {
 		defer reader.Close()
 		im, _, err := image.DecodeConfig(reader)
 		if err != nil {
-			utilsgo.SugarLogger.Error("Error reading image dimensions: ", err)
+			log.Println("Error reading image dimensions: ", err)
 			error = err
 			return
 		}
 		w = im.Width
 		h = im.Height
-		return
 	} else {
-		utilsgo.SugarLogger.Error("unable to open the file:", err)
+		log.Println("unable to open the file:", err)
 		error = err
-		return
 	}
+	return
+}
+
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes.
+func (f *File) copyFile(dst string) error {
+	in, err := os.Open(f.Path)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(fmt.Sprintf("%s/%s", dst, f.Name))
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
--- main.go
+++ main.go
@@ -1,50 +1,109 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"imagesorter/file"
 	"log"
+	"math/big"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/sirajabbas/utilsgo"
 )
 
 func main() {
-	utilsgo.Configure(utilsgo.Config{ErrorFileName: "err.log", LogFileName: "info.log"})
-	args := os.Args[1:]
-	if len(args) == 0 {
-		printHelp()
-		os.Exit(0)
-	}
-	folder := args[0]
 
-	switch folder {
+	/**
+	read the input directory
+	read output dir and create it if not exist
+	read the ratios and process it*/
+	var sourceDir, outputDir, ratioParam string
+
+	sourceDir = readFromConsole("source directory")
+
+	switch sourceDir {
 	case "":
-		log.Fatal("invalid folder name. Try help command")
+		log.Fatal("invalid source directory name. Try help command")
 	case "help":
-		fmt.Println(`
-		imagesorder foldername
-		`)
+		printHelp()
+		return
 	}
-	utilsgo.PrintSuccess("staring application")
+
+	//validating output directory
+	outputDir = readFromConsole("destination directory")
+	if outputDir == "" {
+		log.Fatal("invalid destination directory. Try help command")
+	} else if strings.ToLower(outputDir) == "default" {
+		outputDir = fmt.Sprintf("%s/imagesorter", sourceDir)
+	}
+	//validating ratio param
+	ratioParam = readFromConsole("ratio strings")
+	if ratioParam == "" {
+		log.Fatal("invalid ratio. try help command")
+	} else if strings.ToLower(ratioParam) == "default" {
+		ratioParam = "1:1 4:3 3:4 16:9"
+	}
+
+	//creating destination directory
+	err := utilsgo.CreateDirectory(outputDir + "/unsorted")
 	//creating unsorted directory
-	err := utilsgo.CreateDirectory(folder + "/unsorted")
 	if err != nil {
-		utilsgo.SugarLogger.Error(err)
-		log.Fatal("Error creating unsorted directory")
+		log.Fatal("Error creating destination directory")
 	}
-	utilsgo.PrintSuccess("created unsorted directory")
+	utilsgo.PrintSuccess("created destination directory")
+
 	//stripping last / char
-	if string(folder[len(folder)-1]) == "/" {
-		folder = string(folder[0 : len(folder)-1])
+	if string(sourceDir[len(sourceDir)-1]) == "/" {
+		sourceDir = string(sourceDir[0 : len(sourceDir)-1])
+
 	}
-	fmt.Println(folder)
-	file.SortFiles(folder)
+	//parsing ratios
+	ratioStrings := strings.Split(ratioParam, " ")
+
+	if len(ratioStrings) == 0 {
+		log.Fatal("error processing the ratio")
+	}
+	//processing ratios
+	for _, rs := range ratioStrings {
+		numStrings := strings.Split(rs, ":")
+		if len(numStrings) > 0 && len(numStrings) == 2 {
+			w, err := strconv.Atoi(numStrings[0])
+			h, err := strconv.Atoi(numStrings[1])
+			if err != nil {
+				log.Fatal("error processing ratio")
+			}
+			file.AllowedRatios = append(file.AllowedRatios, new(big.Rat).SetFrac64(int64(w), int64(h)))
+		}
+		//creating destination directory for this ratio
+		fmt.Println("creating directory: ", fmt.Sprintf("%s/%s", outputDir, rs))
+		err = utilsgo.CreateDirectory(fmt.Sprintf("%s/%s", outputDir, rs))
+		if err != nil {
+			log.Fatal("Error creating destination directory.", err)
+		}
+	}
+
+	file.SortFiles(sourceDir, outputDir)
 }
 
 func printHelp() {
-	fmt.Println(`
-		imagesorder foldername
-		imagesorter help
-	`)
+	fmt.Println(`imagesorter`)
+}
+
+func readFromConsole(query string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(query)
+	fmt.Print(":")
+
+	for {
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if text != "" {
+			return text
+		}
+
+	}
 }
